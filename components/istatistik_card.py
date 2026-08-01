6d1306c4dea863231a18d04a6953ea4fab3b9f25
itmaktes-fatih <itmaktes@gmail.com>
2026-08-01 10:43:09 +0300

    Update istatistik_card.py

--- components/istatistik_card.py
+++ components/istatistik_card.py
@@ -1,1 +1,20 @@
+# ==========================================
+# components/istatistik_card.py
+# ==========================================
 
+from kivy.lang import Builder
+from kivymd.uix.card import MDCard
+from kivy.properties import StringProperty
+
+Builder.load_file("components/istatistik_card.kv")
+
+
+class IstatistikCard(MDCard):
+
+    icon = StringProperty("information")
+
+    title = StringProperty("Başlık")
+
+    value = StringProperty("0")
+
+    icon_color = StringProperty("#FFFFFF")
